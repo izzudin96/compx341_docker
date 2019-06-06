3a6c63b8b33c1d13ccdb0b7fc9594b7b152c9f09
Izzudin Anuar <izzudinanuar96@gmail.com>
2019-06-07 11:44:04 +1200

    added first bash script

---
+++ Jmeter/Scenario_1/cpu.1/run.sh
@@ -0,0 +1,4 @@
+jmeter -t Scenario1_test1_delay_.0s_cpu_.1.jmx -n -l /home/ma306/composetest/test_results/test_1.jtl
+jmeter -t Scenario1_test2_delay_.5s_cpu_.1.jmx -n -l /home/ma306/composetest/test_results/test_2.jtl
+jmeter -t Scenario1_test3_delay_1s_cpu_.1.jmx -n -l /home/ma306/composetest/test_results/test_3.jtl
+jmeter -t Scenario1_test4_delay_3s_cpu_.1.jmx -n -l /home/ma306/composetest/test_results/test_4.jtl
