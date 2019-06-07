467167273fbba9621fd3e46467d238102bcd112e
Izzudin Anuar <izzudinanuar96@gmail.com>
2019-06-07 14:59:54 +1200

    all test conducted

--- Jmeter/Scenario_2/cpu.1/run.sh
+++ Jmeter/Scenario_2/cpu.1/run.sh
@@ -1,4 +1,4 @@
-jmeter -t Scenario1_test17_delay_.0s_cpu_.1.jmx -n -l /home/ma306/composetest/test_results/test_17.jtl
-jmeter -t Scenario1_test18_delay_.5s_cpu_.1.jmx -n -l /home/ma306/composetest/test_results/test_18.jtl
-jmeter -t Scenario1_test19_delay_1s_cpu_.1.jmx -n -l /home/ma306/composetest/test_results/test_19.jtl
-jmeter -t Scenario1_test20_delay_3s_cpu_.1.jmx -n -l /home/ma306/composetest/test_results/test_20.jtl
+jmeter -t Scenario2_test17_delay_.0s_cpu_.1.jmx -n -l /home/ma306/composetest/test_results/test_17.jtl
+jmeter -t Scenario2_test18_delay_.5s_cpu_.1.jmx -n -l /home/ma306/composetest/test_results/test_18.jtl
+jmeter -t Scenario2_test19_delay_1s_cpu_.1.jmx -n -l /home/ma306/composetest/test_results/test_19.jtl
+jmeter -t Scenario2_test20_delay_3s_cpu_.1.jmx -n -l /home/ma306/composetest/test_results/test_20.jtl
--- Jmeter/Scenario_2/cpu2.0/run.sh
+++ Jmeter/Scenario_2/cpu2.0/run.sh
@@ -1,4 +1,4 @@
-jmeter -t Scenario1_test25_delay_.0s_cpu_2.0.jmx -n -l /home/ma306/composetest/test_results/test_25.jtl
-jmeter -t Scenario1_test26_delay_.5s_cpu_2.0.jmx -n -l /home/ma306/composetest/test_results/test_26.jtl
-jmeter -t Scenario1_test27_delay_1s_cpu_2.0.jmx -n -l /home/ma306/composetest/test_results/test_27.jtl
-jmeter -t Scenario1_test28_delay_3s_cpu_2.0.jmx -n -l /home/ma306/composetest/test_results/test_28.jtl
+jmeter -t Scenario2_test25_delay_.0s_cpu_2.0.jmx -n -l /home/ma306/composetest/test_results/test_25.jtl
+jmeter -t Scenario2_test26_delay_.5s_cpu_2.0.jmx -n -l /home/ma306/composetest/test_results/test_26.jtl
+jmeter -t Scenario2_test27_delay_1.0s_cpu_2.0.jmx -n -l /home/ma306/composetest/test_results/test_27.jtl
+jmeter -t Scenario2_test28_delay_3.0s_cpu_2.0.jmx -n -l /home/ma306/composetest/test_results/test_28.jtl
